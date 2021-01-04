432c93c4c087c0e0f4058008858de32aa7973747
Artyom <Artyom@example.com>
2021-01-04 18:39:25 +0600

    remove useless things

--- src/components/seo.tsx
+++ src/components/seo.tsx
@@ -1,16 +1,18 @@
-/**
- * SEO component that queries for data with
- *  Gatsby's useStaticQuery React hook
- *
- * See: https://www.gatsbyjs.com/docs/use-static-query/
- */
+import React from 'react'
+import { Helmet } from 'react-helmet'
+import { useStaticQuery, graphql } from 'gatsby'
 
-import React from "react"
-import PropTypes from "prop-types"
-import { Helmet } from "react-helmet"
-import { useStaticQuery, graphql } from "gatsby"
-
-function SEO({ description, lang, meta, title }) {
+function SEO({
+  description = '',
+  lang = 'en',
+  meta = [],
+  title,
+}: {
+  description?: string
+  lang?: string
+  meta?: {}[]
+  title: string
+}) {
   const { site } = useStaticQuery(
     graphql`
       query {
@@ -26,7 +28,7 @@ function SEO({ description, lang, meta, title }) {
   )
 
   const metaDescription = description || site.siteMetadata.description
-  const defaultTitle = site.siteMetadata?.title
+  const defaultTitle: string | undefined = site.siteMetadata?.title
 
   return (
     <Helmet
@@ -34,7 +36,7 @@ function SEO({ description, lang, meta, title }) {
         lang,
       }}
       title={title}
-      titleTemplate={defaultTitle ? `%s | ${defaultTitle}` : null}
+      titleTemplate={defaultTitle ? `%s | ${defaultTitle}` : undefined}
       meta={[
         {
           name: `description`,
@@ -68,22 +70,10 @@ function SEO({ description, lang, meta, title }) {
           name: `twitter:description`,
           content: metaDescription,
         },
-      ].concat(meta)}
+        ...meta,
+      ]}
     />
   )
-}
-
-SEO.defaultProps = {
-  lang: `en`,
-  meta: [],
-  description: ``,
-}
-
-SEO.propTypes = {
-  description: PropTypes.string,
-  lang: PropTypes.string,
-  meta: PropTypes.arrayOf(PropTypes.object),
-  title: PropTypes.string.isRequired,
 }
 
 export default SEO
---
+++ src/pages/index.tsx
@@ -0,0 +1,13 @@
+import React from "react"
+import { Link } from "gatsby"
+
+import SEO from "../components/seo"
+
+const IndexPage = () => (
+  <>
+    <SEO title="Home" />
+    <Link to="/404/">Go to page 404</Link>
+  </>
+)
+
+export default IndexPage
