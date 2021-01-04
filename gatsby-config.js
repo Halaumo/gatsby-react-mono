d5c75fd7cf4e224e78d3b9c2c218436f9dac12e1
Artyom <Artyom@example.com>
2021-01-04 19:48:20 +0600

    add custom eslint

--- gatsby-config.js
+++ gatsby-config.js
@@ -5,6 +5,18 @@ module.exports = {
     author: `Artem`,
   },
   plugins: [
+    {
+      resolve: 'gatsby-plugin-eslint',
+      options: {
+        test: /\.js$|\.jsx$|\.tsx$|\.ts$/,
+        exclude: /(node_modules|.cache|public|.yarn)/,
+        stages: ['develop'],
+        options: {
+          emitWarning: true,
+          failOnError: false,
+        },
+      },
+    },
     `gatsby-plugin-react-helmet`,
     {
       resolve: `gatsby-source-filesystem`,
@@ -19,10 +31,10 @@ module.exports = {
       resolve: `gatsby-plugin-manifest`,
       options: {
         name: `gatsby-starter-default`,
-        short_name: `starter`,
-        start_url: `/`,
-        background_color: `#663399`,
-        theme_color: `#663399`,
+        shortName: `starter`,
+        startUrl: `/`,
+        backgroundColor: `#663399`,
+        themeColor: `#663399`,
         display: `minimal-ui`,
         icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
       },
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,12 +1,12 @@
-import React from "react"
-import { Link } from "gatsby"
+import React from 'react'
+import { Link } from 'gatsby'
 
-import SEO from "../components/seo"
+import SEO from '../components/seo'
 
 const IndexPage = () => (
   <>
-    <SEO title="Home" />
-    <Link to="/404/">Go to page 404</Link>
+    <SEO title='Home' />
+    <Link to='/404/'>Go to page 404</Link>
   </>
 )
 
